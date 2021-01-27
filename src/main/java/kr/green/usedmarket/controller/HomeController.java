1e7567fd33c462c98023ad5ff861e4c26fb75c74
lrw <dlsodnd123@naver.com>
2021-01-27 12:01:50 +0900

    DB연동 설정(MyBatis)

--- src/main/java/kr/green/usedmarket/controller/HomeController.java
+++ src/main/java/kr/green/usedmarket/controller/HomeController.java
@@ -1,4 +1,4 @@
-package kr.green.usedmarket;
+package kr.green.usedmarket.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
