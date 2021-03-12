6951e97c8e6763088eef0c46ba2254a771796091
lrw <dlsodnd123@naver.com>
2021-03-12 17:49:59 +0900

    20210312 DB백업

---
+++ sql/20210312/used_market_chat_room.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE  IF NOT EXISTS `used_market` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `used_market`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: used_market
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chat_room`
+--
+
+DROP TABLE IF EXISTS `chat_room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chat_room` (
+  `chro_num` int NOT NULL,
+  `chro_pd_num` int NOT NULL,
+  `chro_buyer_mb_id` varchar(13) NOT NULL,
+  `chro_seller_mb_id2` varchar(13) NOT NULL,
+  PRIMARY KEY (`chro_num`),
+  KEY `FK_merber_TO_chat_room_2` (`chro_seller_mb_id2`),
+  KEY `FK_merber_TO_chat_room_1` (`chro_buyer_mb_id`),
+  CONSTRAINT `FK_merber_TO_chat_room_1` FOREIGN KEY (`chro_buyer_mb_id`) REFERENCES `member` (`mb_id`),
+  CONSTRAINT `FK_merber_TO_chat_room_2` FOREIGN KEY (`chro_seller_mb_id2`) REFERENCES `member` (`mb_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat_room`
+--
+
+LOCK TABLES `chat_room` WRITE;
+/*!40000 ALTER TABLE `chat_room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chat_room` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-12 17:48:50
---
+++ sql/20210312/used_market_stand.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `used_market` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `used_market`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: used_market
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `stand`
+--
+
+DROP TABLE IF EXISTS `stand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stand` (
+  `st_name` varchar(25) NOT NULL,
+  `st_mb_id` varchar(13) NOT NULL,
+  `st_img` varchar(255) DEFAULT NULL,
+  `st_content` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`st_name`),
+  KEY `FK_merber_TO_stand_1` (`st_mb_id`) /*!80000 INVISIBLE */,
+  CONSTRAINT `FK_merber_TO_stand_1` FOREIGN KEY (`st_mb_id`) REFERENCES `member` (`mb_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stand`
+--
+
+LOCK TABLES `stand` WRITE;
+/*!40000 ALTER TABLE `stand` DISABLE KEYS */;
+INSERT INTO `stand` VALUES ('asdasd123의 가판대','asdasd123','/2021/02/23/a2427cf0-288e-4d76-8f02-9fc8b5245c64_기본이미지.jpg',NULL),('green123의 가판대','green123','/2021/03/11/265eb59c-3df8-4966-8dbe-13a0adff5c1a_샘플이미지10.jpg','qweqwe'),('qweqwe123가판대','qweqwe123','/2021/02/09/42f9ca76-8a26-437f-ae4e-a93ed9cac406_샘플이미지1.jpg','안녕하세요!! 123\n\n\nㅁㅁ'),('rmfls123의 가판대','rmfls123','/2021/02/17/30d8cfb1-64d1-444e-93b6-bbe60276d7f2_샘플이미지10.jpg',NULL);
+/*!40000 ALTER TABLE `stand` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-12 17:48:50
